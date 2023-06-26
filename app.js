395ab5c0ec99e83d6b23b6eda6b168242ba919c0
Vitalii Otiuskyi <vitalii.otiuskyi@gmail.com>
2023-06-26 12:44:31 +0300

    added task 1 & task 2

---
+++ app.js
@@ -0,0 +1,36 @@
+// ----------------------------------------TASK 1----------------------------------------
+console.log(getGreater());
+function getGreater() {
+  let num1 = parseInt(prompt("Input you Number #1!", ""));
+  let num2 = parseInt(prompt("Input you Number #1!", ""));
+
+  if (num1 > num2) {
+    return num1;
+  } else if (num1 < num2) {
+    return num2;
+  } else {
+    alert("The numbers are equal!");
+  }
+}
+
+// ----------------------------------------TASK 2----------------------------------------
+function calcAge(year) {
+  let currentYear = new Date().getFullYear();
+  let age = currentYear - year;
+  return age;
+}
+function yearsUntilRetirement(year, userName) {
+  let age = calcAge(year);
+  let retirementAge = 60;
+
+  if (retirementAge <= age) {
+    console.log(userName + " is already retired.");
+  } else {
+    let yearsUntil = retirementAge - age;
+    console.log(userName + " will retire in " + yearsUntil + " years.");
+  }
+}
+let userName = prompt("What is you name?", "");
+let birthYear = parseInt(prompt("What is your birth year?", ""));
+
+yearsUntilRetirement(birthYear, userName);
